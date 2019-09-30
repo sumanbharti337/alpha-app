5cbd9d9106828e184a2c53baab3eb7d7931e600f
Rails Udemy courses <sumanbharti337@gmail.com>
2019-09-30 15:41:44 +0530

    create show action and add gravatar image using helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,12 +20,16 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Youe account was successfully updated"
+      flash[:success] = "Your account was successfully updated"
       # redirect_to article_path(@article)
       redirect_to articles_path
     else
       render :edit
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
