47e2044118d2e670434078bd40f3e771eedf68f2
Rails Udemy courses <sumanbharti337@gmail.com>
2019-10-01 15:04:33 +0530

    Add categories model and tests

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'bcrypt', '~> 3.1.7'
 #pagination
 gem 'will_paginate', '3.1.7'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'rails-controller-testing'
 # Use mysql as the database for Active Record
 # gem 'mysql2', '>= 0.4.4'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_053744) do
+ActiveRecord::Schema.define(version: 2019_10_01_071258) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2019_10_01_053744) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
